1603aff3d92c67e757c0c1e30883b273c6fb5db7
nata.sesteves@gmail.com <nata.sesteves@gmail.com@548b9d35-5866-3b70-4a50-845a24e847d2>
2014-12-09 23:42:28 +0000

    Manager Script

---
+++ Assets/Script/Manager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Manager : MonoBehaviour {
+
+	public string scene;
+	public float time;
+	float initialtime = 180f;
+	public GUIStyle custom;
+	public float life;
+
+	// Use this for initialization
+	void Start () 
+	{
+		time = initialtime;
+		life = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		time -= Time.deltaTime;
+		Debug.Log( time + "Time");
+
+		if (time <=0)
+		{
+			Application.LoadLevel(scene);
+		}
+	}
+
+	void OnGUI() 
+	{
+
+	GUI.Label (new Rect (Screen.width * 0.7f, Screen.height * 0.01f, 50, 15), "Time: " + time.ToString("F0"), custom);
+	
+	}		
+}
